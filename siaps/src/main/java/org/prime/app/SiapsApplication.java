b382527ddea68d1b15cb9fba42e0c66568c0711f
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2018-07-10 14:23:33 -0300

    #1 - inicializado projeto

---
+++ siaps/src/main/java/org/prime/app/SiapsApplication.java
@@ -0,0 +1,12 @@
+package org.prime.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiapsApplication.class, args);
+	}
+}
